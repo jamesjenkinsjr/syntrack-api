76d6b96a1d82009967c7ed0c20cfbe5cf2e5522e
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-12-17 14:56:33 -0500

    typo

--- src/config.js
+++ src/config.js
@@ -7,5 +7,5 @@ module.exports = {
     'postgresql://dunder-mifflin@localhost/spaced-repetition',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
-  CLIENT_ORIGIN: process.env.CLIENT_URL
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN
 }
